a225142489b415fc6f82e68a51e51f3df20fa044
Namaha-51 <naman.agarwal2024@vitstudent.ac.in>
2026-09-08 13:21:24 +0530

    +91 added to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -60,9 +60,9 @@ export default function Navbar() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-[#F5C518]" viewBox="0 0 20 20" fill="currentColor">
               <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.036 11.036 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
             </svg>
-            <a href="tel:+919821252090" className="text-[#F5C518] hover:text-white transition-colors">9821252090</a>
+            <a href="tel:+919821252090" className="text-[#F5C518] hover:text-white transition-colors">+91 9821252090</a>
             <span className="text-white/30 px-0.5">/</span>
-            <a href="tel:+919920878518" className="text-[#F5C518] hover:text-white transition-colors">9920878518</a>
+            <a href="tel:+919920878518" className="text-[#F5C518] hover:text-white transition-colors">+91 9920878518</a>
           </div>
 
         </div>
